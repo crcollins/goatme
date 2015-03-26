7db13318ec3c46cdf0655781cead65e7effc2fe6
Christopher Collins <crcoll9955@northgeorgia.edu>
2015-03-26 02:05:18 -0400

    Fix CORS headers issues

--- project/settings.py
+++ project/settings.py
@@ -36,6 +36,7 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
 
@@ -47,10 +48,11 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'server',
+    'corsheaders',
 )
-
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
